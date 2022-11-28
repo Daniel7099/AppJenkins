6f9e2507a1aabfab5f21cd07fbb63cd916f63869
Daniel7099 <93681304+Daniel7099@users.noreply.github.com>
2022-11-28 20:21:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 
+<html>
+	<head>
+		<title>Mi página de ejemplo</title>
+	</head>
+	<body>
+	Aquí va el contenido
+    
+     <?php
+            echo "¡Hola, soy un script de PHP!";
+        ?>
+	</body>
+</html>
+
