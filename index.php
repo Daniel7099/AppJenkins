a269d79c9e38656295286773832ddc13d40accb1
Daniel7099 <93681304+Daniel7099@users.noreply.github.com>
2022-12-23 13:59:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 	<?php
  	echo "¡Hola mundoo!";
 
+<div class="box">
+						<div class="box-body" style="overflow-x:auto;">
+						<table class="table is-striped table-bordered table-hover table_id table-condensed "  id="tablaPer">	
+							<thead >	
+								<tr class="info">
+										<th>Nombres</th>
+										<th>Apellidos</th>
+										<th>Email</th>
+										<th>Telefono</th>
+										<th>Telefono2</th>
+										<th>Celular</th>
+										<th>Dirección</th>
+										<th>Empresa//Rubro</th>
+										<th><h6><b>Detalle</b></h6></th>
+										<th>Acciones</th>
+								</tr>
+							</thead>
+							<tbody>
+								
+									</tr>
+
+							</tbody>
+						</table>
+						</div>
+					</div>
